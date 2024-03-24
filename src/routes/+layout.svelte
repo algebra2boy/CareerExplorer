f923824e4d68700f168109e9fecb7634e5adbcfe
Yongye Tan <yongyetan1209@gmail.com>
2024-03-24 02:27:43 -0400

    remove autoInit since we don't it in this simple web app

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,6 @@
 <script>
 	import { afterNavigate } from '$app/navigation';
 	import '../app.css';
-
-	afterNavigate(() => {
-		// @ts-ignore
-		window.HSStaticMethods.autoInit();
-	});
 </script>
 
 <svelte:head>
